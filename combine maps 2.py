04ffccb0af3ee3475af68528548e352c88de46b9
panuscha <charlottepanuskova@gmail.com>
2023-09-25 17:33:46 +0200

    east and west germany, combined map

--- combine maps 2.py
+++ combine maps 2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
+from shapely.ops import cascaded_union
+
 
 # Load the first GeoJSON file (source file with the two polygons to select)
 source_gdf = gpd.read_file('historical-basemaps/geojson/world_1960.geojson')
@@ -23,13 +25,21 @@ combined_gdf = gpd.GeoDataFrame(pd.concat([ not_germany , east_and_west_germany]
 
 gdf1 = gpd.read_file('C:/Users/Panuskova/Nextcloud/translation-mapping/language-basemaps/NUTS_lvl_1.geojson')
 
-belgium_nuts = gdf1[gdf1['NUTS_ID'].isin(['BE1', 'BE2', 'BE3'])]
 not_belgium = combined_gdf[combined_gdf.NAME != "Belgium"]
 
-combined_gdf = gpd.GeoDataFrame(pd.concat([ not_belgium , belgium_nuts], ignore_index=True))
+
+# belgium_nuts = gdf1[gdf1['NUTS_ID'].isin(['BE1', 'BE2', 'BE3'])]
+belgium_fre = gdf1[gdf1['NUTS_ID'].isin(['BE3'])]
+belgium_dut = gdf1[gdf1['NUTS_ID'].isin(['BE1', 'BE2'])]
+belgium_dut_geometry = belgium_dut.unary_union
+belgium_dut_gdf = gpd.GeoDataFrame(geometry=[belgium_dut_geometry])
+belgium_dut_gdf['NAME'] = 'Belgium dut'
+
+combined_gdf = gpd.GeoDataFrame(pd.concat([ not_belgium , belgium_dut_gdf, belgium_fre ], ignore_index=True))
 combined_gdf.loc[combined_gdf.NUTS_ID == 'BE3','NAME'] = 'Belgium fre'
-combined_gdf.loc[combined_gdf.NUTS_ID == 'BE2','NAME'] = 'Belgium dut'
-combined_gdf.loc[combined_gdf.NUTS_ID == 'BE1','NAME'] = 'Belgium dut'
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'BE2','NAME'] = 'Belgium dut'
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'BE1','NAME'] = 'Belgium dut'
+combined_gdf.iloc[240]['NAME'] = 'Belgium dut'
 
 # Save the modified target file as a new GeoJSON file
 #target_gdf.to_file('modified_target.geojson', driver='GeoJSON')
@@ -38,16 +48,33 @@ combined_gdf.loc[combined_gdf.NUTS_ID == 'BE1','NAME'] = 'Belgium dut'
 
 gdf1 = gpd.read_file('C:/Users/Panuskova/Nextcloud/translation-mapping/language-basemaps/NUTS_lvl_2.geojson')
 
-switzerland_nuts = gdf1[gdf1['NUTS_ID'].str.startswith('CH')]
+#switzerland_nuts = gdf1[gdf1['NUTS_ID'].str.startswith('CH')]
 not_switzerland = combined_gdf[combined_gdf.NAME != "Switzerland"]
 
-combined_gdf = gpd.GeoDataFrame(pd.concat([ not_switzerland , switzerland_nuts], ignore_index=True))
-combined_gdf.loc[combined_gdf.NUTS_ID == 'CH01','NAME'] = 'Switzerland fre'
-combined_gdf.loc[combined_gdf.NUTS_ID == 'CH02','NAME'] = 'Switzerland fre'
-combined_gdf.loc[combined_gdf.NUTS_ID == 'CH03','NAME'] = 'Switzerland ger'
-combined_gdf.loc[combined_gdf.NUTS_ID == 'CH04','NAME'] = 'Switzerland ger'
-combined_gdf.loc[combined_gdf.NUTS_ID == 'CH05','NAME'] = 'Switzerland ger'
-combined_gdf.loc[combined_gdf.NUTS_ID == 'CH06','NAME'] = 'Switzerland ger'
+
+## French swiss
+switzerland_fre = gdf1[gdf1['NUTS_ID'].isin(['CH01', 'CH02'])]
+switzerland_fre_geometry = switzerland_fre.unary_union
+switzerland_fre_gdf = gpd.GeoDataFrame(geometry=[switzerland_fre_geometry])
+switzerland_fre_gdf['NAME'] = 'Switzerland fre'
+
+## German swiss
+switzerland_ger = gdf1[gdf1['NUTS_ID'].isin(['CH03', 'CH04', 'CH05', 'CH06'])]
+switzerland_ger_geometry = switzerland_ger.unary_union
+switzerland_ger_gdf = gpd.GeoDataFrame(geometry=[switzerland_ger_geometry])
+switzerland_ger_gdf['NAME'] = 'Switzerland ger'
+
+##Italian swiss
+switzerland_it = gdf1[gdf1['NUTS_ID'].isin(['CH07'])]
+ 
+combined_gdf = gpd.GeoDataFrame(pd.concat([ not_switzerland , switzerland_fre_gdf, switzerland_ger_gdf, switzerland_it], ignore_index=True))
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH01','NAME'] = 'Switzerland fre'
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH02','NAME'] = 'Switzerland fre'
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH03','NAME'] = 'Switzerland ger'
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH04','NAME'] = 'Switzerland ger'
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH05','NAME'] = 'Switzerland ger'
+# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH06','NAME'] = 'Switzerland ger'
+
 combined_gdf.loc[combined_gdf.NUTS_ID == 'CH07','NAME'] = 'Switzerland it'
 
 # Add more GeoDataFrames as needed
---
+++ save_new_country.py
@@ -0,0 +1,125 @@
+import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
+import os
+import re
+import csv
+
+
+def get_closest_lower_year(my_list, year):
+    left, right = 0, len(my_list) - 1
+    closest_lower = None
+
+    while left <= right:
+        mid = (left + right) // 2
+        if my_list[mid] <= year:
+            closest_lower = my_list[mid]
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return closest_lower
+
+
+folder_path = "C:/Users/Panuskova/Nextcloud/translation-mapping/historical-basemaps/geojson"
+
+# List all files in the folder
+files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+print(files)
+
+pattern = r'world_(\w{4})\.geojson'
+
+# Get 4 digit year from the files
+file_years = list(map(lambda x: re.search(pattern, x).group(1) if re.search(pattern, x) else None , files)) 
+# Drop None
+file_years = list(filter(lambda x: x is not None, file_years))
+# Convert to integer
+file_years = list(map(lambda x: int(x), file_years))
+
+print(file_years)
+geotagged_df = pd.read_csv("geotagged/geotagged_germany.csv")
+
+saved_country_dict = {}
+
+geotagged_df = geotagged_df.sort_values(by=['year'])
+
+country_names = []
+map_years = []
+
+not_found_cities = {}
+try:
+    for i, geo_row in geotagged_df.iterrows():
+        # Get year from the geotagged dataframe
+        year = geo_row['year']
+
+        # Find out the closest lower year 
+        map_year = get_closest_lower_year(file_years, year)
+
+        dict_key = (geo_row['geonames_name'], map_year)
+
+        # 
+        try:
+            if saved_country_dict.get(dict_key) is None:
+            
+                # Path to geojson
+                #geojson_path = folder_path + '/world_' + str(map_year)+ '.geojson'
+                geojson_path = 'combined.geojson'
+
+                # Load the basemap GeoJSON file into a GeoDataFrame
+                historical_basemap = gpd.read_file(geojson_path)
+
+                # Get coordinates from the geotagged df
+                longitude = geo_row['geonames_lng']
+                latitude = geo_row['geonames_lat']
+
+                # Create a Point geometry object from the coordinates
+                point = Point(longitude, latitude)
+
+                found = False
+                # Iterate over each country's geometry and check if the point is within it
+                for index, row in historical_basemap.iterrows():
+                    if row['geometry'].contains(point):
+                        country_name = row['NAME']
+                        print(f"{geo_row['geonames_name']} was in {str(year)} in {country_name}.")
+                        saved_country_dict[dict_key] = country_name
+                        found = True
+                        break
+            else:
+                country_name =  saved_country_dict[dict_key]
+                found = True
+
+            map_years.append(map_year) 
+
+            if found:    
+                country_names.append(country_name)     
+            else:
+                k = (i, geo_row['geonames_name'], geo_row['year'])
+                if geo_row['geonames_country'] in historical_basemap['NAME'].values:
+                    country_names.append(geo_row['geonames_country'])
+                    saved_country_dict[dict_key] = country_name
+                    not_found_cities[k] = geo_row['geonames_country']
+                else:
+                    country_names.append(None) 
+                    not_found_cities[k] = None               
+        except:
+            country_names.append(None)    
+            map_years.append(None)      
+except:
+    print("error on row {i}")
+finally:
+    geotagged_df['map_year'] = map_years
+    geotagged_df['region_country_name'] = country_names
+    geotagged_df.to_csv("geotagged/geotagged_germany_country.csv")
+
+    flattened_data = [(key[0], key[1], key[2], value) for key, value in not_found_cities.items()]
+    
+    with open('cities_not_found_new.csv', 'w', newline='', encoding = 'utf8') as file:
+        writer = csv.writer(file)
+        # Write the header row
+        writer.writerow(['Index', 'City', 'Year', 'Country'])
+        # Write the data rows
+        writer.writerows(flattened_data)
+
+
