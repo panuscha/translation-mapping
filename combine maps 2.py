d094421f8bb7f0ecefcd2f75ceefccd0a7ce00ea
panuscha <charlottepanuskova@gmail.com>
2023-10-01 15:39:51 +0200

    Catalonia

--- combine maps 2.py
+++ combine maps 2.py
@@ -7,6 +7,10 @@ from shapely.ops import cascaded_union
 # Load the first GeoJSON file (source file with the two polygons to select)
 source_gdf = gpd.read_file('historical-basemaps/geojson/world_1960.geojson')
 
+
+
+
+
 #### EAST AND WEST GERMANY #####
 
 # Load the second GeoJSON file (target file where you want to replace one polygon)
@@ -21,28 +25,58 @@ east_and_west_germany = source_gdf.iloc[143:145]
 not_germany = target_gdf[target_gdf.NAME != "Germany"]
 combined_gdf = gpd.GeoDataFrame(pd.concat([ not_germany , east_and_west_germany], ignore_index=True))
 
+
+
+
 ##### BELGIUM #####
 
+# Load GeoJSON file with NUTS 1
 gdf1 = gpd.read_file('C:/Users/Panuskova/Nextcloud/translation-mapping/language-basemaps/NUTS_lvl_1.geojson')
 
+# Define area that is not Belgium 
 not_belgium = combined_gdf[combined_gdf.NAME != "Belgium"]
 
-
-# belgium_nuts = gdf1[gdf1['NUTS_ID'].isin(['BE1', 'BE2', 'BE3'])]
+# Get french speaking part of Belgium
 belgium_fre = gdf1[gdf1['NUTS_ID'].isin(['BE3'])]
+
+# Get flemish speaking part of Belgium 
 belgium_dut = gdf1[gdf1['NUTS_ID'].isin(['BE1', 'BE2'])]
+# Combine to one geometry
 belgium_dut_geometry = belgium_dut.unary_union
+# Create GeoDataFrame
 belgium_dut_gdf = gpd.GeoDataFrame(geometry=[belgium_dut_geometry])
 belgium_dut_gdf['NAME'] = 'Belgium dut'
 
+# Combine the rest of the world and flemish and french part of Belgium into one GeoDataFrame 
 combined_gdf = gpd.GeoDataFrame(pd.concat([ not_belgium , belgium_dut_gdf, belgium_fre ], ignore_index=True))
+# Name
 combined_gdf.loc[combined_gdf.NUTS_ID == 'BE3','NAME'] = 'Belgium fre'
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'BE2','NAME'] = 'Belgium dut'
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'BE1','NAME'] = 'Belgium dut'
 combined_gdf.iloc[240]['NAME'] = 'Belgium dut'
 
-# Save the modified target file as a new GeoJSON file
-#target_gdf.to_file('modified_target.geojson', driver='GeoJSON')
+
+
+
+##### SPAIN ######
+
+# Define area that is not Spain 
+not_spain = combined_gdf[combined_gdf.NAME != "Spain"]
+
+# Get spanish speaking part of the world
+spain_esp = gdf1[gdf1['NUTS_ID'].isin(['ES1', 'ES2', 'ES3', 'ES4', 'ES6', 'ES7'])]
+# Combine into one geometry
+spain_esp_geometry = spain_esp.unary_union
+# Create GeoDataFrame
+spain_esp_gdf = gpd.GeoDataFrame(geometry=[spain_esp_geometry])
+spain_esp_gdf['NAME'] = 'Spain'
+
+# Get catalonian speaking part of the world
+spain_cat = gdf1[gdf1['NUTS_ID'].isin(['ES5'])]
+
+# Combine the rest of the world and spanish and catalonian part of Spain into one GeoDataFrame 
+combined_gdf = gpd.GeoDataFrame(pd.concat([ not_spain, spain_esp_gdf, spain_cat ], ignore_index=True))
+combined_gdf.loc[combined_gdf.NUTS_ID == 'ES5','NAME'] = 'Catalonia'
+
+
 
 ##### SWITZERLAND ####
 
@@ -50,7 +84,6 @@ gdf1 = gpd.read_file('C:/Users/Panuskova/Nextcloud/translation-mapping/language-
 
 #switzerland_nuts = gdf1[gdf1['NUTS_ID'].str.startswith('CH')]
 not_switzerland = combined_gdf[combined_gdf.NAME != "Switzerland"]
-
 
 ## French swiss
 switzerland_fre = gdf1[gdf1['NUTS_ID'].isin(['CH01', 'CH02'])]
@@ -68,14 +101,8 @@ switzerland_ger_gdf['NAME'] = 'Switzerland ger'
 switzerland_it = gdf1[gdf1['NUTS_ID'].isin(['CH07'])]
  
 combined_gdf = gpd.GeoDataFrame(pd.concat([ not_switzerland , switzerland_fre_gdf, switzerland_ger_gdf, switzerland_it], ignore_index=True))
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH01','NAME'] = 'Switzerland fre'
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH02','NAME'] = 'Switzerland fre'
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH03','NAME'] = 'Switzerland ger'
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH04','NAME'] = 'Switzerland ger'
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH05','NAME'] = 'Switzerland ger'
-# combined_gdf.loc[combined_gdf.NUTS_ID == 'CH06','NAME'] = 'Switzerland ger'
-
 combined_gdf.loc[combined_gdf.NUTS_ID == 'CH07','NAME'] = 'Switzerland ita'
+
 
 # Add more GeoDataFrames as needed
 combined_gdf.to_file('language-basemaps/combined.geojson', driver='GeoJSON')
