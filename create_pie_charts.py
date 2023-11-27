c302b45a868a8e30acc51986ddca89b6eef0df84
panuscha <charlottepanuskova@gmail.com>
2023-11-27 12:33:34 +0100

    pie charts
    based on:
    LIMIT = 0.02
    TOP = 3
    MIN_TRANS = 4

--- create_pie_charts.py
+++ create_pie_charts.py
@@ -2,7 +2,9 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.colors as mcolors
+from matplotlib import colors
+from matplotlib import colormaps
+from webcolors import rgb_to_name
 
 # all plot years
 plot_years = ['1918', '1929', '1945', '1956', '1967','1978', '1989', '2000', '2011' ] 
@@ -11,11 +13,17 @@ plot_years = ['1918', '1929', '1945', '1956', '1967','1978', '1989', '2000', '20
 df = pd.read_excel("D:\\Panuskova\\Nextcloud\\translation-mapping\\translation-mapping\\weights\\translations_language_countries.xlsx")
 
 # all countries and languages
-countries = df.country.unique()
-languages = df.language.unique()
+#countries = df.country.unique()
+# languages = df.language.unique()
+
+#countries = ["USSR", "Switzerland", "Slovakia", "Italy", "Germany", "Spain", "United Kingdom", "Sweden", "Denmark", "Poland",  "Austria", "Czechoslovakia", "Yugoslavia", "Belgium", "Hungary"]
+countries =["Italy", "Belgium", "United Kingdom", "Czechoslovakia", "Denmark", "Sweden", "Slovakia", "Spain", "Switzerland", "USSR", "Germany (Soviet)", "East Germany",  "Germany", "Yugoslavia", "Poland", "Austria", "Italy", "Hungary"]
+languages = ["rus", "est", "arm", "glg", "ukr", "wen", "eng", "lit", "baq", "dut", "slv", "mac", "hrv", "epo", "hun", "ger", "cat", "fre", "slo", "other"]
+
 
 my_color_palette = {}
-all_colors = list(mcolors.CSS4_COLORS.keys())
+#all_colors = list(colors.CSS4_COLORS.keys())
+all_colors =[plt.cm.tab20(i) for i in range(20)]
 
 # Save color for each country
 for i, language in enumerate(languages):
@@ -42,7 +50,7 @@ for plot_year in plot_years:
             
             id = h.index[h.weights == h_max]
             
-            outer_colors = [my_color_palette[h.language[id[0]]] , 'black' ]
+            outer_colors = [my_color_palette['eng'] , 'black' ]
 
             weights_all = [h_max, sum(h.weights) - h_max ]
             
@@ -55,9 +63,19 @@ for plot_year in plot_years:
             h = h[h.weights != h_max]
 
             if not(h.empty):
-
+                
+                if len(h.weights) > 3 or any(i <= 4 for i in h.weights):
+                    h = h.sort_values(by = 'weights', ascending = 0)
+                    weights_sorted = list(h.weights)
+                    ind = list(map(lambda i: i <= 4, weights_sorted)).index(True)
+                    ind = min([3, ind])
+                    df2_dict = {'country': country, 'language': "other", 'map_year': plot_year, 'weights': sum(weights_sorted[ind:]) }
+                    df2 = pd.DataFrame(data = df2_dict, index = ['0'])
+                    h = h.head(ind)
+                    h = pd.concat([h, df2], ignore_index = True)
+                    
                 # get all colors for languages 
-                inner_colors = [my_color_palette[language] for language in h.language  ]
+                inner_colors = [my_color_palette[l] for l in h.language  ]
 
                 # get all languages 
                 language = h.language
@@ -65,11 +83,15 @@ for plot_year in plot_years:
                 # if only weights, change to h.weights
                 weights = [w/sum_weights for w in h.weights] 
 
-                ax.pie(weights, radius=1-size, colors=inner_colors,
+                wedges, _ = ax.pie(weights, radius=1-size, colors=inner_colors,
                 wedgeprops=dict(edgecolor='w')) #width=size, 
-
+                ax.legend(wedges, language,
+                    title="languages",
+                    loc="center left",
+                    bbox_to_anchor=(1, 0, 0.5, 1))
                 plt.title("{country}_{year}".format(country = country, year = plot_year))
-                plt.show()
+                
+                #plt.show()
                 #hist = h[['language','weights']].plot(kind = 'bar', figsize=(8, 6), x = 'language', ylim = [0, y_max], color = colors).get_figure()
                 plt.savefig('D:\\Panuskova\\Nextcloud\\translation-mapping\\translation-mapping\\plots\\histograms minor percentage pie charts\\{country}_{year}'.format(country = country, year = plot_year))
                 plt.close()
--- number_of_colors.py
+++ number_of_colors.py
@@ -4,16 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.colors as mcolor
 
-LIMIT = 0.05
+LIMIT = 0.02
+TOP = 3
+MIN_TRANS = 4
 
-# all plot years
-plot_years = ['1918', '1929', '1945', '1956', '1967','1978', '1989', '2000', '2011' ] 
+
 
 # Table with number of translations for each country, language and decade
 df = pd.read_excel("D:\\Panuskova\\Nextcloud\\translation-mapping\\translation-mapping\\weights\\translations_language_countries.xlsx")
 
+# all plot years
+plot_years = df.map_year.unique()#['1918', '1929', '1945', '1956', '1967','1978', '1989', '2000', '2011' ] 
+
 # all countries and languages
-countries = ["Finland", "Ukraine", "Italy", "Lithuania" ,"Yugoslavia",  "Germany (USA)",  "Denmark",  "Austria", "Portugal", "Belgium",  "Czechoslovakia", "Byelarus", "United Kingdom", "Iceland", "Poland", "Germany", "Albania", "Romania","Ireland", "Slovakia", "Netherlands", "Latvia", "Switzerland",  "Hungary", "Sweden" , "USSR",  "Greece", "Spain", "France", "Bosnia and Herzegovina", "Norway"] 
+countries = list(df.country.unique())#["Finland", "Ukraine", "Italy", "Lithuania" ,"Yugoslavia",  "Germany (USA)",  "Denmark",  "Austria", "Portugal", "Belgium",  "Czechoslovakia", "Byelarus", "United Kingdom", "Iceland", "Poland", "Germany", "Albania", "Romania","Ireland", "Slovakia", "Netherlands", "Latvia", "Switzerland",  "Hungary", "Sweden" , "USSR",  "Greece", "Spain", "France", "Bosnia and Herzegovina", "Norway", "West Germany"] 
+countries.remove('Czech Republic')
+countries.remove('Japan')
 #df.country.unique() 
 languages = df.language.unique()
 
@@ -41,19 +47,16 @@ for plot_year in plot_years:
                 # Discard the most common language (= should be the major language)
                 h = h[h.weights != weights_max]
                 
-                max_id = min([len(h), 3])
+                max_id = min([len(h), TOP])
                 
                 try:
-                
                     for i in range(max_id):
-                    
                         weights_max = max(h.weights)
-                        if weights_max > 4 :
+                        if weights_max > MIN_TRANS :
                             id = h.index[h.weights == weights_max]
                             needed_languages.add(h.language[id[0]])
                             needed_countries.add(country)
                             h = h[h.weights != weights_max]
-                        
                 except:
                     print(country + str(plot_year))
                         
